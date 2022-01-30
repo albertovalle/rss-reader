cf3b62a30e9da9d96f5925ddfd99f9d5ea7fe792
albertovalle <57776571+albertovalle@users.noreply.github.com>
2022-01-30 16:11:25 +0100

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
-
-//componetne funcional puto y creamos funcion con arrow fuction, utilizar evaulaciones dentro del elemento el tittle lo recuparmos de las propos
-//hacemos desestructiracion de forma que nuestro componetne quede mas legilebl
-//usamos  prop especial children titulo sera lo que envolvamos en nuestro componente title
-// se exporta constante con nombre Title y el valor es la arrow function que tenemos-
-//usamos bulma para estilar estilos nuestros componentes
-//componente de intercafe
-//bulma lo usamos en el searhForm
+//Hacemos desestructiracion de forma que nuestro componente quede mas legible usando la prop especial children, que será lo que envolvamos en nuestro componente title
+// Se exporta la constante con nombre Title y el valor, es la arrow function que tenemos.
 export const Title= ({ children }) => (
     <h1 className="title">{children}</h1>
 )
